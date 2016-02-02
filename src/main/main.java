39db86e9eb5bb53999dcd1ffbbbdd2173f41cf01
Wesley Leach <leachw9001@gmail.com>
2016-02-01 18:03:00 -0800

    s

--- src/main/main.java
+++ src/main/main.java
@@ -5,13 +5,51 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Vector;
 
-import hard.LongestRepeatedString;
-import hard.PALINDROMIC_RANGES;
 
-public class main {
+public class Main {
 	public static Vector<String> inputs;
+	
+	public static int sumOfArray(int[] arrayInput){
+		int sum=0;
+		for( int part : arrayInput)sum += part;
+		return sum;
+	}
+	
+	public static void largestSubSetSum(){
+		for(String line : Main.inputs){
+			String[] numberStrings = line.split(",");
+			int[] numbers = new int[numberStrings.length];
+			// Fills the new int array
+			for(int x=0; x<numbers.length;x++){
+				numbers[x] = Integer.getInteger(numberStrings[x]);
+			}
+			int largestSum=0,tempSum=0;
+			// Begin calculating largest sum here
+			for(int i=0; i <1000000; i++)
+			// Example input -10,2,3,-2,0,5,-15
+			Random random = new Random();
+			int firstEndPoint = random.nextInt(numbers.length);
+			int secondEndPoint = random.nextInt(numbers.length);
+			
+			if(firstEndPoint > secondEndPoint){
+				int temp=secondEndPoint;
+				secondEndPoint = firstEndPoint;
+				firstEndPoint = temp;
+			}
+			int[] subAry = Arrays.copyOfRange(numbers, firstEndPoint , secondEndPoint);
+			tempSum = sumOfArray(subAry);
+			// Reassigns largest Sum
+			if(tempSum > largestSum) largestSum = tempSum;
+			}
+		
+			
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		try{
@@ -22,7 +60,8 @@ public class main {
 				inputs.add(lineInput);
 			}
 			br.close();
-			LongestRepeatedString func = new LongestRepeatedString();
+			//Put the function call here
+			
 		}
 		catch(FileNotFoundException exception){
 			System.out.println(exception);
