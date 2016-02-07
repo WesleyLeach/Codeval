ec58895b4fe790860645f34fc531f5dabe8f957b
Wesley Leach <leachw9001@gmail.com>
2016-02-06 23:39:02 -0800

    yiu

--- src/main/main.java
+++ src/main/main.java
@@ -15,7 +15,6 @@ public class Main {
 	
 	
 	public static void LongestRepeatedString(){
-		Vector<String> possibleStrings = new Vector<String>();
 		for( String Line :  Main.inputs){
 			
 			// BRUTE FORCE
@@ -30,23 +29,19 @@ public class Main {
 			Vector<String> subStrings = new Vector<String>();
 			String lineSaver = Line;
 			
-			// Goes to the left
-			for (int x =0; x<= Line.length(); x++){
-				// removes the first element and adds the remainder to the Vector
-				Line = Line.substring(0, x);
-				subStrings.add(Line);
-				
-			}
-			Line = lineSaver;
 			// Goes to the right
-			for (int x =0; x<= Line.length(); x++){
+			
+			Line = lineSaver;
+			char[] temp = Line.toCharArray();
+			for (int x =0; x< Line.length(); x++){
 				// removes the first element and adds the remainder to the Vector
-				Line = Line.substring(x, 0);
-				subStrings.add(Line);
 				
 			}
-
-			for (String output : subStrings) System.out.println(output);
+			
+			
+			
+			for (String output : subStrings) 
+				if( !output.equals(null) )System.out.println(output);
 			
 		}
 		
@@ -64,7 +59,6 @@ public class Main {
 			}
 			br.close();
 			//Put the function call here
-			
 			LongestRepeatedString();
 			
 			
