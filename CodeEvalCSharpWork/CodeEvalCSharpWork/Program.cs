e70bec168609c1886072034e7b4edf2e8d260d6f
WesleyLeach <leachw9001@gmail.com>
2016-02-07 21:27:16 -0800

    Working on  C# practice

--- CodeEvalCSharpWork/CodeEvalCSharpWork/Program.cs
+++ CodeEvalCSharpWork/CodeEvalCSharpWork/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CodeEvalCSharpWork
 {
     class Program
@@ -18,7 +19,40 @@ namespace CodeEvalCSharpWork
                 {
                     // Read the stream to a string, and write the string to the console.
                     String line = sr.ReadToEnd();
-                    Console.WriteLine(line);
+					String[] lines = line.Split('\n');
+					foreach( String singleLine in lines){
+						// Where you start processing each line
+						Console.WriteLine ("Got to 24");
+						// splits target and inputs
+						String[] twoParts = singleLine.Split (';');
+
+						String[] numberLines = twoParts[1].Split(',');
+						Console.WriteLine (numberLines.Length);
+						int[] numbers = new int[ numberLines.Length-1 ];
+						int target = int.Parse (twoParts[1]);
+						Console.WriteLine ("Got to 28");
+						// Fills the numbers array
+						for(int x=0; x < numbers.Length; x++){
+							numbers[x] = int.Parse (numberLines[x]);
+						}
+						// Arrays filled, time to start building a list of pairs
+						int  temp=0;
+						Console.WriteLine ("Got to 34");
+						List<String> outputs = new List<String>();
+						for(int x=0; x<numbers.Length;x++){
+							temp=numbers[x];
+							for(int i=0; i<numbers.Length;i++){
+								if( temp + numbers[i] == target){
+									outputs.Add (numbers[x] +","+numbers[i]);
+								}
+							}
+						}
+						Console.WriteLine ("Got to 49");
+						foreach(String print in outputs){
+							Console.WriteLine (print);
+						}
+
+					}
                 }
             }
             catch (Exception e)
