c600654560845709e27c0a8852feb78a7a500c56
Wesley Leach <leachw9001@gmail.com>
2016-02-10 10:22:00 -0800

    Longest String mostly Working

--- src/hard/LongestRepeatedString.java
+++ src/hard/LongestRepeatedString.java
@@ -21,26 +21,102 @@ NONE
 
 public class LongestRepeatedString {
 
-	public LongestRepeatedString(){
-		Vector<String> Lines = Main.inputs;
-		Vector<String> possibleStrings = new Vector<String>();
-		for( String Line : Lines){
-			// BRUTE FORCE
-			// So the idea is to slice the string into all possible substrings
-			// Then sort the vector that contains substrings by length
-			// go down the least in descending order, test if sub string is in inputed string
-			// print first string to be in input string and exit
-			Vector<String> subStrings = new Vector<String>();
-			String lineSaver = Line;
-			for (int x =1; x<= Line.length(); x++){
-				// removes the first element and adds the remainder to the Vector
-				Line = Line.substring(x);
-				subStrings.addElement(Line);
-			}
-			for (String output : subStrings) System.out.println(output);
+	public static void LongestRepeatedString(){
+		for( String Line :  Main.inputs){
 			
+			// BRUTE FORCE
+			// Move		Move	Move	
+			//	right	left	-> <-
+			// banana	banana	banana	banana	b		ba	    ban		bana	banan
+			//	anana	banan	anan	 anana	ba		ban 	bana	banan	banana
+			//   nana	bana	na		  anan	ban		bana	banan	banana
+			//    ana	ban				   nan	bana	banan	banana
+			//     na	ba				    an	banan	banana
+			//      a	b						banana
+			//	
+			//	a		an		ana		anan	
+			//	an		ana		anan	anana
+			//	ana		anan	anana
+			//	anan	anana
+			//	anana
+			
+			Vector<String> subStrings = new Vector<String>();
+			Line = Line.trim();
+			
+			for( int x=0 ; x < Line.length() ; x++){
+				for( int i=x; i< Line.length(); i ++){
+					if( x+i <= Line.length()){
+						String sub = Line.substring(x, x+i);
+						subStrings.add(sub);
+					}
+				}
+			}
+			// Get duplicates
+			Vector<String> duplicatedStrings = new Vector<String>();
+			char[] tempLine = Line.toCharArray();
+			boolean foundOnce =false,founceTwice=false;
+			int startForSecond;
+			
+			for( int i=0; i< subStrings.size(); i++){
+				char[] tempCompare = subStrings.get(i).toCharArray();
+				// Checks for the first duplicate
+				// Runs for each word
+
+				foundOnce=false;
+				founceTwice=false;
+				if (tempCompare.length>0)
+				for(int y=0; y<tempLine.length;y++){
+					
+					if(tempCompare[0] == tempLine[y] && !foundOnce){
+						for(int x=1; x< tempCompare.length; x++){
+							if( y+x < tempLine.length)
+							if(tempCompare[x] ==tempLine[y+x]){
+								if(x == tempCompare.length-1){
+
+									foundOnce =true;
+									y= y+x;
+								}
+							}
+						}
+					}
+					
+					else if(tempCompare[0] == tempLine[y] && foundOnce){
+						for(int x=1; x< tempCompare.length; x++){
+							if( y+x < tempLine.length)
+							if(tempCompare[x] ==tempLine[y+x]){
+								if(x == tempCompare.length-1){
+									founceTwice =true;
+									y= y+x;
+									break;
+								}
+							}
+						}
+					}
+				}
+				
+				if(foundOnce && founceTwice)duplicatedStrings.add(subStrings.get(i));
+					
+			}
+			
+			//Find Longest
+			String longestString = null;
+			if(duplicatedStrings.size() == 0){
+				System.out.println("NULL");
+			}
+			else{
+				longestString = duplicatedStrings.elementAt(0);
+				for(String testString : duplicatedStrings){
+					if( longestString.length() < testString.length()){
+						longestString = testString;
+					}
+				}
+				if(longestString.equals(" "))System.out.println("NULL");
+				else System.out.println(longestString);
+			}
 			
 		}
+			
+		
 		
 	}
 
--- src/main/main.java
+++ src/main/main.java
@@ -20,48 +20,93 @@ public class Main {
 			// BRUTE FORCE
 			// Move		Move	Move	
 			//	right	left	-> <-
-			// banana	banana	banana	banana
-			//	anana	banan	anan	 anana
-			//   nana	bana	na		  anan
-			//    ana	ban				   nan
-			//     na	ba				    an
-			//      a	b
+			// banana	banana	banana	banana	b		ba	    ban		bana	banan
+			//	anana	banan	anan	 anana	ba		ban 	bana	banan	banana
+			//   nana	bana	na		  anan	ban		bana	banan	banana
+			//    ana	ban				   nan	bana	banan	banana
+			//     na	ba				    an	banan	banana
+			//      a	b						banana
+			//	
+			//	a		an		ana		anan	
+			//	an		ana		anan	anana
+			//	ana		anan	anana
+			//	anan	anana
+			//	anana
+			
 			Vector<String> subStrings = new Vector<String>();
 			Line = Line.trim();
+			Line = Line.toLowerCase();
 			for( int x=0 ; x < Line.length() ; x++){
-				for( int i=0; i< Line.length(); i ++){
+				for( int i=x; i< Line.length(); i ++){
 					if( x+i <= Line.length()){
 						String sub = Line.substring(x, x+i);
 						subStrings.add(sub);
 					}
 				}
 			}
+			
 			// Get duplicates
 			Vector<String> duplicatedStrings = new Vector<String>();
+			char[] tempLine = Line.toCharArray();
+			boolean foundOnce =false,founceTwice=false;
+			int startForSecond;
+			
 			for( int i=0; i< subStrings.size(); i++){
-				for(int x=0; x< subStrings.size(); x++){
-					if( i != x){
-						if( subStrings.elementAt(i).equals(subStrings.elementAt(x)) ){
-							duplicatedStrings.add(subStrings.elementAt(i));
+				char[] tempCompare = subStrings.get(i).toCharArray();
+				// Checks for the first duplicate
+				// Runs for each word
+
+				foundOnce=false;
+				founceTwice=false;
+				if (tempCompare.length>0)
+				for(int y=0; y<tempLine.length;y++){
+					
+					if(tempCompare[0] == tempLine[y] && !foundOnce){
+						for(int x=1; x< tempCompare.length; x++){
+							if( y+x < tempLine.length)
+							if(tempCompare[x] ==tempLine[y+x]){
+								if(x == tempCompare.length-1){
+
+									foundOnce =true;
+									y= y+x+1;
+								}
+							}
+						}
+					}
+					
+					else if(tempCompare[0] == tempLine[y] && foundOnce){
+						for(int x=1; x< tempCompare.length; x++){
+							if( y+x < tempLine.length)
+							if(tempCompare[x] ==tempLine[y+x]){
+								if(x == tempCompare.length-1){
+									founceTwice =true;
+									y= y+x+1;
+									break;
+								}
+							}
 						}
 					}
 				}
+				
+				if(foundOnce && founceTwice)duplicatedStrings.add(subStrings.get(i));
 					
 			}
 			
 			//Find Longest
 			String longestString = null;
-			if(duplicatedStrings.size() == 0 ){
-				System.out.println("NULL");
+			if(duplicatedStrings.size() == 0){
+				System.out.println("NONE");
 			}
 			else{
+				// finds longest string, if equal it ignores it
 				longestString = duplicatedStrings.elementAt(0);
 				for(String testString : duplicatedStrings){
 					if( longestString.length() < testString.length()){
 						longestString = testString;
 					}
 				}
-				System.out.println(longestString);
+				if(longestString.equals(" "))System.out.println("NONE");
+				else System.out.println(longestString);
 			}
 			
 		}
