531a0db1daa3271cbe82df47105fb3395c2c1930
Wesley Leach <leachw9001@gmail.com>
2016-02-10 05:59:50 -0800

    Created too good of a sub-strining algorithm
    Need to make it so it wont accidentally get strings in the reverse order
    or something. Too tired to try and articulate

--- src/main/main.java
+++ src/main/main.java
@@ -27,12 +27,41 @@ public class Main {
 			//     na	ba				    an
 			//      a	b
 			Vector<String> subStrings = new Vector<String>();
-
+			Line = Line.trim();
 			for( int x=0 ; x < Line.length() ; x++){
 				for( int i=0; i< Line.length(); i ++){
-					String sub = Line.substring(x, x+i);
-					subStrings.add(sub);
+					if( x+i <= Line.length()){
+						String sub = Line.substring(x, x+i);
+						subStrings.add(sub);
+					}
 				}
+			}
+			// Get duplicates
+			Vector<String> duplicatedStrings = new Vector<String>();
+			for( int i=0; i< subStrings.size(); i++){
+				for(int x=0; x< subStrings.size(); x++){
+					if( i != x){
+						if( subStrings.elementAt(i).equals(subStrings.elementAt(x)) ){
+							duplicatedStrings.add(subStrings.elementAt(i));
+						}
+					}
+				}
+					
+			}
+			
+			//Find Longest
+			String longestString = null;
+			if(duplicatedStrings.size() == 0 ){
+				System.out.println("NULL");
+			}
+			else{
+				longestString = duplicatedStrings.elementAt(0);
+				for(String testString : duplicatedStrings){
+					if( longestString.length() < testString.length()){
+						longestString = testString;
+					}
+				}
+				System.out.println(longestString);
 			}
 			
 		}
