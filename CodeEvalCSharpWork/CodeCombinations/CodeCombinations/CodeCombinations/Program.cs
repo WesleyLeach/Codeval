6a3c5365c0ef247cf82eb65c67cbc691e14c9948
Wesley Leach <leachw9001@gmail.com>
2016-06-05 11:48:28 -0700

    km

---
+++ CodeEvalCSharpWork/CodeCombinations/CodeCombinations/CodeCombinations/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCombinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader fileInput = new StreamReader(args[0]))
+                {
+                    String fileContent = fileInput.ReadToEnd();
+                    String[] lines = fileContent.Split('\n');
+
+                    foreach (String line in lines)
+                    {
+                        // Main loop here
+                        String[] splitLines = line.Split('|');
+                        for
+                        Console.WriteLine(splitLines);
+                    }
+                }
+            }
+            catch( Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
