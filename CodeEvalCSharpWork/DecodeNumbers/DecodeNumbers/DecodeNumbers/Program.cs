77344fe2752aa046fa02da4d73b89d8cf5413306
Wesley Leach <leachw9001@gmail.com>
2016-06-06 14:50:04 -0700

    Finished Locked Doors
    Finished Locked Doors

---
+++ CodeEvalCSharpWork/DecodeNumbers/DecodeNumbers/DecodeNumbers/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecodeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader fileReader = new StreamReader(args[0]))
+                {
+                    String inputText = fileReader.ReadToEnd();
+                    String[] lines = inputText.Split('\n');
+
+                    foreach(String line in lines)
+                    {
+                       
+                        Console.WriteLine(number_of_combinations(line.Trim()));
+                    }
+                }
+                
+            }
+            catch( Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static int number_of_combinations(String raw_input)
+        {
+            int combinations = 0, x=0;
+            Boolean alreadyConsumed = false;
+            while(x < raw_input.Length)
+            {
+                if( raw_input[x].Equals('1') && (x+1 <raw_input.Length) && !alreadyConsumed)
+                {
+                    if (raw_input[x + 1].Equals('2') || raw_input[x + 1].Equals('1'))
+                    {
+                        alreadyConsumed = true;
+                        combinations += 2;
+                        x+=2;
+                    }
+                    else
+                    {
+                        combinations += 2;
+                        x++;
+                    }
+
+                }
+                else if (raw_input[x].Equals('2') && (x + 1 < raw_input.Length) && !alreadyConsumed)
+                {
+                    if (raw_input[x + 1].Equals('2') || raw_input[x + 1].Equals('1'))
+                    {
+                        alreadyConsumed = true;
+                        combinations += 2;
+                        x+=2;
+                    }
+                    else
+                    {
+                        combinations += 2;
+                        x++;
+                    }
+
+                }
+                else if(!raw_input[x].Equals('0'))
+                {
+                    if(raw_input[x].Equals('1') || raw_input[x].Equals('2'))
+                    {
+                        if (!alreadyConsumed)
+                        {
+                            combinations++;
+                            x++;
+                        }
+                        else
+                        {
+                            x++;
+                        }
+                    }
+                    else
+                    {
+                        combinations += 1;
+                        x++;
+                    }
+
+                }
+                else
+                {
+                    x++;
+                }
+            }
+            
+            return combinations;
+        }
+    }
+}
---
+++ CodeEvalCSharpWork/Locks/Locks/Locks/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader fileReader = new StreamReader(args[0]))
+                {
+                    String inputText = fileReader.ReadToEnd();
+                    String[] lines = inputText.Split('\n');
+
+                    foreach (String line in lines)
+                    {
+                        String[] values = line.Split(' ');
+
+                        // True == locked
+                        Boolean[] doors = new Boolean[Int32.Parse( values[0])];
+                        int passes = Int32.Parse(values[1]),runs=0, unlocked_doors = 0;
+
+                        Boolean evenDoor = false;
+                        while (runs < passes - 1)
+                        {
+                            // Even's Pass
+                            for (int x = 0; x < doors.Length; x++)
+                            {
+                                if (x % 2 != 0) doors[x] = true;
+                            }
+                            // 3's Pass
+                            for (int x = 0; x < doors.Length; x++)
+                            {
+                                if (x % 3 == 2) doors[x] = !doors[x];
+                            }
+                            runs++;
+                        }
+                        doors[doors.Length - 1] = !doors[doors.Length - 1];
+
+                        foreach( Boolean door in doors)
+                        {
+                            if (!door) unlocked_doors++;
+                        }
+                        Console.WriteLine(unlocked_doors);
+                    }// End of main loop
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    
+    }
+}
