8f5e7e833e94bf2ec9d9746f2afd6ea327b0e23e
Wesley Leach <leachw9001@gmail.com>
2016-06-06 09:58:38 -0700

    Finished CodeCombinations

--- CodeEvalCSharpWork/CodeCombinations/CodeCombinations/CodeCombinations/Program.cs
+++ CodeEvalCSharpWork/CodeCombinations/CodeCombinations/CodeCombinations/Program.cs
@@ -22,8 +22,17 @@ namespace CodeCombinations
                     {
                         // Main loop here
                         String[] splitLines = line.Split('|');
-                        for
-                        Console.WriteLine(splitLines);
+                        int numberOfCodes = 0;
+                        // Trims out white space
+                        for (int x = 0; x < splitLines.Length; x++) splitLines[x] = splitLines[x].Trim();
+                        // Pull the code out for each line
+
+                        for(int x =0; x < splitLines.Length-1; x++)
+                        {
+                            numberOfCodes += contains_code(splitLines[x], splitLines[x + 1]);
+                        }
+                        Console.WriteLine(numberOfCodes);
+
                     }
                 }
             }
@@ -32,6 +41,28 @@ namespace CodeCombinations
                 Console.WriteLine("The file could not be read:");
                 Console.WriteLine(e.Message);
             }
+        }
+
+        static int contains_code(string lineOne,string lineTwo)
+        {
+            int codeCount = 0;
+            String testString = "";
+            for( int x=0; x<lineOne.Length-1; x++)
+            {
+                testString = "";
+                testString = testString + lineOne[x];
+                testString = testString + lineOne[x+1];
+                testString = testString + lineTwo[x];
+                testString = testString + lineTwo[x+1];
+                testString = testString.ToLower();
+                if (testString.Contains('c') && testString.Contains('o') && testString.Contains('d') && testString.Contains('e'))
+                {
+                    codeCount++;
+                }
+            }
+
+
+            return codeCount;
         }
     }
 }
