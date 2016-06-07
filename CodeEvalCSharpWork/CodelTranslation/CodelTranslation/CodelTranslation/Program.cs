e9beef9504031511fea960927468581b024b5c25
Wesley Leach <leachw9001@gmail.com>
2016-06-06 20:14:22 -0700

    Push

--- CodeEvalCSharpWork/CodelTranslation/CodelTranslation/CodelTranslation/Program.cs
+++ CodeEvalCSharpWork/CodelTranslation/CodelTranslation/CodelTranslation/Program.cs
@@ -19,7 +19,7 @@ namespace CodelTranslation
                     String[] lines = input.Split('\n');
                     foreach(String line in lines)
                     {
-
+                        Console.WriteLine(translate(line));
                     }// End of main loop;
                 }
             }
@@ -31,15 +31,21 @@ namespace CodelTranslation
 
         static String translate(String input_string)
         {
-            String newString="";
             char[] alphabet = {'a', 'b', 'c', 'd','e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z' };
-            char[] decode_alphabet = { 'y', 'h', 'e', 's', 'o', 'c', 'g', 'h', 'd', 'u', 'i', 'g', 'l', 'b', 'k', 'r', 'q', 't', 'n', 'w', 'j', 'p', 'f', 'm', 'a', 'q' };
+            char[] decode_alphabet = { 'y', 'h', 'e', 's', 'x', 'v', 'g', 'h', 'd', 'u', 'i', 'g', 'l', 'b', 'k', 'r', 'z', 't', 'n', 'w', 'j', 'p', 'f', 'm', 'a', 'q' };
             char[] char_array = input_string.ToLower().ToCharArray();
-            for( int x=0; x < char_array.Length; x++)
+
+            for (int y= 0; y < char_array.Length;y++)
             {
-                
+                for(int x = 0; x < alphabet.Length; x++)
+                {
+                    if (char_array[y] == alphabet[x]){
+                        char_array[y] = decode_alphabet[x];
+                        break;
+                    }
+                }
             }
-            return newString;
+            return new string(char_array);
         }
     }
 }
